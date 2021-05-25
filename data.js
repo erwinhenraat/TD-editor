ee8f01f37f39e83c678019690b3cd8ed3fbb3ee3
Erwin Henraat <e.henraat@ma-web.nl>
2021-05-25 16:44:05 +0200

    basic grid functions

---
+++ data.js
@@ -0,0 +1,203 @@
+var data = {
+        "dimensionX" : 10,
+        "dimensionY" : 10,
+        "cellWidth" : 10,
+        "cellHeight" : 10,
+        "tiles": [
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "blocked"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "waypoint"
+          },
+          {
+            "state": "spawn"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "blocked"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "waypoint"
+          },
+          {
+            "state": "spawn"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "blocked"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "waypoint"
+          },
+          {
+            "state": "spawn"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "blocked"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "waypoint"
+          },
+          {
+            "state": "spawn"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "blocked"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "waypoint"
+          },
+          {
+            "state": "spawn"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "blocked"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "waypoint"
+          },
+          {
+            "state": "spawn"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "blocked"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "waypoint"
+          },
+          {
+            "state": "spawn"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "free"
+          },
+          {
+            "state": "blocked"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "path"
+          },
+          {
+            "state": "waypoint"
+          },
+          {
+            "state": "spawn"
+          }
+        ]
+      }
+    
+
+  console.log(JSON.stringify(data));
